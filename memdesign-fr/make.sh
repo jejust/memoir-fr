ceb4edc8113cd2f601029e8749634c498cd4fe9b
Jérémy Just <jeremy@jejust.fr>
2017-10-10 23:54:02 +0200

    Utilisation de "paracol" pour avoir une colonne en anglais et une colonne en français. Feu tricolore pour indiquer l'état de la traduction. Le balisage avec "paracol" est terminé dans "memdesign" mais pas dans "memman".

---
+++ memdesign-fr/make.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+pdflatex memdesign-fr
+pdflatex memdesign-fr
+
+bibtex memdesign-fr
+
+pdflatex memdesign-fr
+
+bibtex memdesign-fr
+
+pdflatex memdesign-fr
+pdflatex memdesign-fr
+
